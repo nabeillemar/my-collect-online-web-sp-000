0d2a13886901bc2f19e888c7648774f2b5ce006b
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-01 14:00:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -11,9 +11,4 @@ end
 
 students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
-puts my_collect(students){ |name| puts name.split(" ").first }
-
-#my_collect(students) do |student|
-     # student.split(" ").first
-    #end
-    #expect(students).to eq(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'])
+puts my_collect(students){|name| puts name.split(" ").first}
